48ad74d144f9a419ecbfcdb018fa6cfb7ea1da0f
Dmitry Golub <>
2021-03-17 12:33:19 +0300

    init

---
+++ Sources/App/routes.swift
@@ -0,0 +1,30 @@
+import Vapor
+
+private let savedReviewers = ["Dmitry Holub", "Aliaksandr Batyesheu", "Andrei Olesau",
+                              "Anna Kazhuro", "Ihar Maroz", "Vlad Kubicki", "Maksim Panamarou",
+                              "Natalia Semicheva", "Tatsiana Gouteeva", "Vladimir Martyniuk", "Yan Schneider"]
+private var reviewers: [String] = []
+
+func routes(_ app: Application) throws {
+    
+    app.get { req -> String in
+        return getReviewers()
+    }
+}
+
+func getReviewers() -> String {
+    var reviewersString = ""
+    if reviewers.count < 2 {
+        reviewers = savedReviewers
+    }
+    let firstReviewer = Int.random(in: 0..<reviewers.count)
+    reviewersString = reviewers[firstReviewer]
+    reviewers.remove(at: firstReviewer)
+    var secondReviewer = Int.random(in: 0..<reviewers.count)
+    while firstReviewer == secondReviewer {
+        secondReviewer = Int.random(in: 0..<reviewers.count)
+    }
+    reviewersString += " and " + reviewers[secondReviewer]
+    reviewers.remove(at: secondReviewer)
+    return reviewersString
+}
